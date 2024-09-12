5b613eeebcd305c1766f584d2f51c047151da0dd
Tornikezhuzhniashvili <senate2507@gmail.com>
2024-09-12 21:13:28 +0400

    third commit

---
+++ day3/homework/python_lesson3.py
@@ -0,0 +1,13 @@
+name="tornike"
+print("my name is", name)
+
+age="16"
+
+print("i am", age)
+
+favourite_food="khinkali"
+
+favourite_color="red"
+
+print("my favourite food is", favourite_food)
+print("my favourite color is", favourite_color)
