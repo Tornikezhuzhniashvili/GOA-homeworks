f6dc2c7f91214205d0f725f7f96c9979b7187208
Tornikezhuzhniashvili <senate2507@gmail.com>
2025-02-13 17:14:04 +0400

    commit

---
+++ Day 42/Homework/00.py
@@ -0,0 +1,5 @@
+def sum_mix(arr):
+    result = 0
+    for num in arr:
+        result += int(num)
+    return result
---
+++ Day 42/Homework/04.py
@@ -0,0 +1,12 @@
+def sum_str(a, b):
+    if a == "":
+        num1 = 0
+    else:
+        num1 = int(a)
+    if b == "":
+        num2 = 0
+    else:
+        num2 = int(b)
+    
+    sum = num1 + num2
+    return str(sum)
