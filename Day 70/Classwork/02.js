aced6fa510b68a9cb4b082fd03d9da5566c15ad5
Tornikezhuzhniashvili <senate2507@gmail.com>
2025-05-11 16:02:28 +0400

    commit

---
+++ Day 70/Classwork/02.js
@@ -0,0 +1,6 @@
+function ChangeTxt(){
+    let h1 = document.getElementById("h_1")
+    h1.innerText = "hello world!!!"
+}
+    
+
---
+++ Day 70/Classwork/level70.js
@@ -0,0 +1,11 @@
+let name = "tornike"
+let surname = "zhuzhniashvili"
+let age = 16
+let height = 183
+let weight = 75
+
+console.log(name)
+console.log(surname)
+console.log(age)
+console.log(height)
+console.log(weight)
